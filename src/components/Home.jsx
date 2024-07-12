db8d2cd62dfbab48227690503a9acd93bd1f0fe7
Bhuwan Thapa <thapabhuwan15@gmail.com>
2024-07-13 00:29:37 +0545

    styled the play button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,27 @@
 const Home = ({ onGame }) => {
   return (
     <div className="home">
-      <div className="title">TYPING GAME</div>
+      <div className="title">
+        TYPING QUEST: <p>An Adventure Awaits</p>
+      </div>
+      <br />
       <div className="author">
         Coding & <br />
-        Design by Lun Dev
+        Design by Bhuwan Thapa
       </div>
-      <button onClick={() => onGame("playing")} className="btnPlay">
-        Play Game
-      </button>
+      <div className="buttonContainer">
+        <button onClick={() => onGame("playing")} className="btnPlay">
+          P L A Y
+          <div id="clip">
+            <div id="leftTop" class="corner"></div>
+            <div id="rightBottom" class="corner"></div>
+            <div id="rightTop" class="corner"></div>
+            <div id="leftBottom" class="corner"></div>
+          </div>
+          <span id="rightArrow" class="arrow"></span>
+          <span id="leftArrow" class="arrow"></span>
+        </button>
+      </div>
     </div>
   );
 };
